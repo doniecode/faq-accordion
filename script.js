2624a7c7b3734cad1dc78fae47c148388cdce9c3
Donald Zarura <150556665+doniecode@users.noreply.github.com>
2024-07-15 18:07:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const faqs = document.querySelectorAll("details");
-const questions = document.querySelectorAll('summary');
-const answers = document.querySelectorAll("details .answer");
+const details = document.querySelectorAll('details');
+
+details.forEach((detail) => {
+  detail.querySelector('summary').addEventListener('click', () => {
+    details.forEach((d) => {
+      if (d !== detail) {
+        d.removeAttribute('open');
+      }
+    });
+  });
+});
